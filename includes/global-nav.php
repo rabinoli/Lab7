723f62631c4d20eef016541608010daf6e2f5c1e
RabinOli <157316784+rabinoli@users.noreply.github.com>
2024-03-04 16:00:49 -0500

    Add files via upload

---
+++ includes/global-nav.php
@@ -0,0 +1,11 @@
+<nav>
+				<ul>
+					<li><a href="/index.php" title="Go to the Home page">Home</a></li>
+					<li><a href="/red.php" title="Learn about red blocks">Red Blocks</a></li>
+					<li><a href="/blue.php" title="Learn about blue blocks">Blue Blocks</a></li>
+					<li><a href="/yellow.php" title="Learn about yellow blocks">Yellow Blocks</a></li>
+					<li><a href="/green.php" title="Learn about green blocks">Green Blocks</a></li>
+					<!-- <li><a href="/subscribe.php" title="Learn about subscribe blocks">subscribe </a></li> -->
+					<li><a href="/about.php" title="Learn more about us">About</a></li>
+				</ul>
+			</nav>
